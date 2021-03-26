a24b90fe7c10f8c16213003cd163e9bef01eda7c
Nejah <nejahyengui999@gmail.com>
2021-03-26 10:00:03 +0100

    jsx

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import First from './Components/First' ;
+import Secand from './Components/Secand' ;
+import Third from './Components/Third' ;
+import Image from './Components/Image';
+import Videos from './Components/Videos';
+
+function App() {
+  return (
+    <div className="App">
+      <First />
+      <Image/>
+      <Secand />
+      <Third />
+      <Videos/>
+     
+      
+ 
+  </div>
+  );
+}
+
+export default App;
